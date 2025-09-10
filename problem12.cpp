8bc6136ce5e4696b0c0519c3ea6bc71c7021d542
250261-tech <250261@newuu.uz>
2025-09-10 13:06:48 +0500

    .

--- problem12.cpp
+++ problem12.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 int main() {
     int a=241;
-
+    int r=a%11;
+    cout<<"In the first group: "<<int(a/11)<<endl;
+    cout<<"In the second group: "<<int(r)<<endl;
+    return 0;
 }
