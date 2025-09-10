f3a79869118252536e9ae99b471f11d878cea43e
250261-tech <250261@newuu.uz>
2025-09-10 11:43:08 +0500

    .

---
+++ problem1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int value1;
+    int value2;
+    cin >> value1 >> value2;
+    cout << value1 + value2 << endl;
+}
---
+++ problem2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Shaxriyor Dongboyev on 10/09/25.
+//
